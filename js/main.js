20d3333fe456da92a23d77f77d47c675cff5da57
marcinj <marcin.jurewicz@gmail.com>
2022-07-28 10:26:24 +0200

    rozwiazanie zadania #39354 js-tablica-ze-zmiennymi

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let length = '5469';
+console.log(length);
+
+let width = '7645'
+console.log(width)
+
+let depth = '1567';
+console.log(depth);
+
+let array = [5469, 7645, 1567];
+console.log(array);
+
+
+
